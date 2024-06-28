b5eb577cdaaa5bbda2c4636e88d31c948cce6b23
Gregory Paccaud <gpaccaud@gmail.com>
2024-06-28 18:32:57 +0200

    update section component

--- stories/components/section-prefooter/section-prefooter.stories.js
+++ stories/components/section-prefooter/section-prefooter.stories.js
@@ -1,5 +1,3 @@
-// import './_section.scss';
-
 import sectionPFTemplate from './section-prefooter.html.twig';
 import sectionPFDocs from '!!raw-loader!./section-prefooter.docs.mdx';
 import sectionPFSource from '!!raw-loader!./section-prefooter.html.twig';
@@ -7,6 +5,7 @@ import sectionPFSource from '!!raw-loader!./section-prefooter.html.twig';
 
 export default {
   title: 'Components/Section/Section pre-Footer',
+  component: 'Section', 
 	parameters: {
 		componentSubtitle:
      'This is the pre-footer section.',
@@ -23,13 +22,6 @@ export default {
   args: {
   },
   argTypes: {
-    class: {
-      control: 'text', 
-      description: 'Some classes.',
-      type: {
-        required: false,
-      }
-    },
     heading: { 
       control: 'text', 
       description: 'the heading (h1, hx) component.',
@@ -53,7 +45,7 @@ const Template = (args) => sectionPFTemplate(args);
 export const Example = Template.bind({});
 Example.args = {
   heading: '<h2 class="text-left">My section title</h2>',
-  content: '<div class="col-md-6 align-self-center">   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  <a href="https://example.com" class="btn btn-white shadow btn-lg mt-2 mb-4" title="Primary link title" target="_self">Main link color</a>   </div>    <div class="col-md-6 align-self-center">      <img src="https://picsum.photos/seed/picsum/1920/1080" width="1920" height="1080" class="img-fluid rounded shadow" alt="Descriptive Alt Text"></div>',
+  content: '<div class="col-md-6 align-self-center">   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  <a href="https://example.com" class="btn btn-white shadow btn-lg mt-2" title="Primary link title" target="_self">Main link color</a>   </div>    <div class="col-md-6 mt-4 mt-md-0 align-self-center"><img src="https://picsum.photos/seed/picsum/1920/1080" width="1920" height="1080" class="img-fluid rounded shadow" alt="Descriptive Alt Text"></div>',
 };
 Example.parameters = {
 	// backgrounds: { default: 'dark' },
--- stories/components/section/section.stories.js
+++ stories/components/section/section.stories.js
@@ -6,7 +6,7 @@ import sectionSource from '!!raw-loader!./section.html.twig';
 
 
 export default {
-  title: 'Components/Section/Section default',
+  title: 'Components/Section/Section',
 	parameters: {
 		componentSubtitle:
      'Section is just a <section> with some parameters for design (class and background). Title is optional (h2 element) and the content is to be define.',
@@ -54,7 +54,7 @@ export const Example = Template.bind({});
 Example.args = {
   class: "position-relative p-5 bg-secondary text-bg-primary bg-gradient",
   heading: '<h2 class="h1 text-center">My section title</h2>',
-  content: '<div class="col-md-6">   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  <a href="https://example.com" class="btn btn-white shadow btn-lg mt-2 mb-4" title="Primary link title" target="_self">Main link color</a>   </div>    <div class="col-md-6">      <img src="https://picsum.photos/seed/picsum/1920/1080" width="1920" height="1080" class="img-fluid rounded shadow" alt="Descriptive Alt Text"></div>',
+  content: '<div class="col-md-6">   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  <a href="https://example.com" class="btn btn-white shadow btn-lg mt-2 " title="Primary link title" target="_self">Main link color</a>   </div>    <div class="col-md-6 mt-4 mt-md-0">      <img src="https://picsum.photos/seed/picsum/1920/1080" width="1920" height="1080" class="img-fluid rounded shadow" alt="Descriptive Alt Text"></div>',
 };
 Example.parameters = {
 	// backgrounds: { default: 'dark' },

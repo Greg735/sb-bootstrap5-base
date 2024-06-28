857fb789a0f0d276e616aaec34dd379885a4b416
Gregory Paccaud <gpaccaud@gmail.com>
2024-06-28 06:11:02 +0200

    add buttons group component

---
+++ stories/components/buttons-group/button-group.stories.js
@@ -0,0 +1,34 @@
+import buttonGroupTemplate from './button-group.html.twig';
+import ButtonGroupSource from '!!raw-loader!./button-group.html.twig';
+
+
+export default {
+  title: 'Components/Button/Group',
+  parameters: {
+    componentSource: {
+      code: ButtonGroupSource,
+      language: 'twig',
+    },
+    controls: { disable: true },
+  },
+  argTypes: {
+    content: {
+        table: {
+            disable: true,
+        }
+    },
+  },
+};
+
+
+const Template = (args) => buttonGroupTemplate(args);
+
+export const Default = Template.bind({});
+Default.args = {
+  content: '<button type="button" class="btn btn-outline-primary">First action</button><button type="button" class="btn btn-outline-primary">Second action</button>'
+}
+
+export const DefaultTree = Template.bind({});
+DefaultTree.args = {
+  content: '<button type="button" class="btn btn-primary">First action</button><button type="button" class="btn btn-secondary">Second action</button><button type="button" class="btn btn-danger">Third action</button>'
+}

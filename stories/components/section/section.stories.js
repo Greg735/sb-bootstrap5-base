ed91a36e0afc269151f56acdea7ff418fa03e71d
Gregory Paccaud <gpaccaud@gmail.com>
2024-06-28 16:26:00 +0200

    add new section

--- stories/components/section/section.stories.js
+++ stories/components/section/section.stories.js
@@ -6,7 +6,7 @@ import sectionSource from '!!raw-loader!./section.html.twig';
 
 
 export default {
-  title: 'Components/Section',
+  title: 'Components/Section/Section default',
 	parameters: {
 		componentSubtitle:
      'Section is just a <section> with some parameters for design (class and background). Title is optional (h2 element) and the content is to be define.',
@@ -53,8 +53,8 @@ const Template = (args) => sectionTemplate(args);
 export const Example = Template.bind({});
 Example.args = {
   class: "position-relative p-5 bg-secondary text-bg-primary bg-gradient",
-  heading: '<h2 class="text-center">My section title</h2>',
-  content: '<div class="col-md-6 align-self-center">   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  <a href="https://example.com" class="btn btn-white shadow btn-lg mt-2 mb-4" title="Primary link title" target="_self">Main link color</a>   </div>    <div class="col-md-6 align-self-center">      <img src="https://picsum.photos/seed/picsum/1920/1080" width="1920" height="1080" class="img-fluid rounded shadow" alt="Descriptive Alt Text"></div>',
+  heading: '<h2 class="h1 text-center">My section title</h2>',
+  content: '<div class="col-md-6">   <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>      <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>  <a href="https://example.com" class="btn btn-white shadow btn-lg mt-2 mb-4" title="Primary link title" target="_self">Main link color</a>   </div>    <div class="col-md-6">      <img src="https://picsum.photos/seed/picsum/1920/1080" width="1920" height="1080" class="img-fluid rounded shadow" alt="Descriptive Alt Text"></div>',
 };
 Example.parameters = {
 	// backgrounds: { default: 'dark' },

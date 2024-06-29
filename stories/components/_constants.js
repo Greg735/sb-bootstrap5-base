99478c02ee580cbbec6567da12929a7864f9d400
Gregory Paccaud <gpaccaud@gmail.com>
2024-06-29 12:42:42 +0200

    add constants files

---
+++ stories/components/_constants.js
@@ -0,0 +1,42 @@
+export const constants = {
+    colors : {
+        actions : {
+            options: [
+                'success',
+                'warning',
+                'danger',
+                'info',
+            ],
+        },
+        theme : {
+            options: [
+                'primary', 
+                'secondary', 
+                'blue', 
+                'gray', 
+                'green', 
+                'red', 
+                'orange', 
+                'cyan', 
+                'light', 
+                'dark', 
+                'white', 
+                'black'
+            ] 
+        }
+    },
+    button : {
+        type: [
+            'button',
+            'submit',
+            'reset',
+        ]
+    },
+    sizes: {
+        options: [
+            'sm',
+            'default',
+            'lg',
+        ]
+    }
+}

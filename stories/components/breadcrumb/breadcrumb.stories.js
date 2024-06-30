dc2b55b28bf5898893e9f56625314805f00d87ee
Gregory Paccaud <gpaccaud@gmail.com>
2024-06-30 21:33:26 +0200

    add dist, breadcrubm and pagination

---
+++ stories/components/breadcrumb/breadcrumb.stories.js
@@ -0,0 +1,81 @@
+
+import BreadcrumbTemplate from './breadcrumb.html.twig';
+import BreadcrumbDocs from '!!raw-loader!./breadcrumb.docs.mdx';
+import BreadcrumbSource from '!!raw-loader!./breadcrumb.html.twig';
+
+export default {
+  title: 'Components/Breadcrumb',
+	parameters: {
+		componentSubtitle: '',
+    componentSource: {
+      code: BreadcrumbSource,
+      language: 'twig',
+    },
+    docs: {
+      description: {
+        component: BreadcrumbDocs,
+      },
+    },
+	},
+  args: {
+  },
+  argTypes: {
+    breadcrumb: {
+        control: 'object',
+        description: 'Array of breadcrumb items with text and optional URL',
+        table: {
+          type: { summary: 'object' },
+        },
+      },
+  },
+};
+
+const Template = (args) => BreadcrumbTemplate(args);
+
+export const Default = Template.bind({});
+Default.args = {
+    breadcrumb: [
+        { text: 'Home', url: '/' },
+        { text: 'Library', url: '/library' },
+        { text: 'Data', url: '/library/data' },
+        { text: 'Current Page' },
+    ],
+};
+Default.parameters = {
+	docs: {
+		description: {
+            story : 'Displays a breadcrumb with multiple items, some with links and some without.',
+        }
+	},
+}
+  
+export const SingleItem = Template.bind({});
+SingleItem.args = {
+  breadcrumb: [
+    { text: 'Home', url: '/' },
+  ],
+};
+SingleItem.parameters = {
+	docs: {
+		description: {
+            story : 'Displays a breadcrumb with a single item.',
+        }
+	},
+}
+
+export const WithoutLinks = Template.bind({});
+WithoutLinks.args = {
+  breadcrumb: [
+    { text: 'Home' },
+    { text: 'Library' },
+    { text: 'Data' },
+    { text: 'Current Page' },
+  ],
+};
+WithoutLinks.parameters = {
+	docs: {
+		description: {
+            story : 'Displays a breadcrumb with multiple items, none of which are links.',
+        }
+	},
+}
---
+++ stories/components/pagination/pagination.stories.js
@@ -0,0 +1,78 @@
+
+import PaginationTemplate from './pagination.html.twig';
+import PaginationDocs from '!!raw-loader!./pagination.docs.mdx';
+import PaginationSource from '!!raw-loader!./pagination.html.twig';
+
+
+export default {
+  title: 'Components/Pagination',
+	parameters: {
+		componentSubtitle:
+     '',
+    componentSource: {
+      code: PaginationSource,
+      language: 'twig',
+    },
+    docs: {
+      description: {
+        component: PaginationDocs,
+      },
+    },
+	},
+  args: {
+  },
+  argTypes: {
+    pagination_title: { 
+      control: 'text', 
+      type: {
+        required: true,
+      }
+    },
+    // highlight: { 
+    //   control: 'boolean',
+    //   table: {
+    //     type: { summary: 'boolean' },
+    //     defaultValue: { summary: false },
+    //   }
+    // },
+    // type: {
+    //   control: { type: 'select'},
+    //   defaultValue: "primary", 
+    //   options: ['success', 'danger', 'warning', 'info'],
+    //   table: {
+    //     require: "true",
+    //     type: { summary: 'string' },
+    //   },
+    //   if: { 
+    //     arg: 'highlight' 
+    //   },
+    // },
+    // subtle: {
+    //   control: 'boolean',
+    //   table: {
+    //     type: { summary: 'boolean' },
+    //     defaultValue: { summary: false },
+    //   },
+    //   if: { 
+    //     arg: 'highlight' 
+    //   },
+    // },
+    // content: { 
+    //   control: 'text', 
+    //   type: {
+    //     required: true,
+    //   }
+    // },
+  },
+};
+
+const Template = (args) => PaginationTemplate(args);
+
+export const Simple = Template.bind({});
+Simple.args = {
+  pagination_title: 'Search results pages',
+  // highlight: false,
+  // type: null,
+  // subtle: false,
+  // content: 'Integer risus velit, imperdiet eu gravida vel, vestibulum et eros. Donec mollis est nulla, eget tincidunt quam lobortis non. Phasellus pellentesque eget lorem in fringilla. Cras eget risus elit. Curabitur mollis eget risus eu tempus. Pellentesque id eleifend enim. Quisque facilisis molestie tellus. Suspendisse nec vehicula velit, eu pellentesque elit. Curabitur a egestas leo. Praesent sed augue tortor. In ac ex ut nibh sollicitudin faucibus non ac nibh. Quisque augue arcu, volutpat ut justo bibendum, dictum blandit purus. Nulla ornare nibh a pellentesque auctor. Suspendisse porta turpis non mauris tristique, id commodo tortor tincidunt. Vestibulum ut dui et enim lacinia condimentum.',
+};

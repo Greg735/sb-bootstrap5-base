0dc430e49489833551451ebc44807e1694a0a4c0
Gregory Paccaud <gpaccaud@gmail.com>
2024-09-16 17:56:24 +0200

    update fondation headings

--- stories/foundations/headings/headings.stories.js
+++ stories/foundations/headings/headings.stories.js
@@ -1,4 +1,4 @@
-import TwigHeadings from './headings.twig'
+import TwigHeadings from './headings.local.twig'
 import HeadingsDocs from '!!raw-loader!./headings.docs.mdx'
 
 
@@ -12,29 +12,6 @@ export default {
 			},
 		  },
 	},
-	argTypes: {
-		heading_level: {
-			name: 'Heading level',
-			description: 'Semantic heading level (h1-h6)',
-			control: {
-				type: 'range',
-				min: 1,
-				max: 6,
-				step: 1,
-			},
-		},
-		text: {
-			name: 'Heading text',
-			description: 'Sample text for heading',
-			control: {
-				type: 'text',
-			},
-		},
-	},
-	args: { heading_level: 1, text: 'Sample heading text' },
 }
 
-const Template = ({ heading_level, text }) =>
-	TwigHeadings({ heading_level, text })
-
-export const Headings = Template.bind({})
+export const Example = (args) => TwigHeadings(args);

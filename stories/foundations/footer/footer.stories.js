77f95c9d7685097ac001da32ad73afd8a64a144d
Gregory Paccaud <gpaccaud@gmail.com>
2025-01-25 10:32:40 +0100

    add footer foundation

---
+++ stories/foundations/footer/footer.stories.js
@@ -0,0 +1,23 @@
+import TwigFooter from './footer.local.twig'
+import TwigFooter2 from './footer2.local.twig'
+import TwigFooter3 from './footer3.local.twig'
+import TwigFooter4 from './footer4.local.twig'
+import FooterDocs from '!!raw-loader!./footer.docs.mdx'
+
+
+export default {
+	title: 'Foundations/Footer',
+	parameters: {
+		componentSubtitle: 'HTML footer',
+		docs: {
+			description: {
+			  component: FooterDocs,
+			},
+		  },
+	},
+}
+
+export const Example = (args) => TwigFooter(args);
+export const Example2 = (args) => TwigFooter2(args);
+export const Example3 = (args) => TwigFooter3(args);
+export const Example4 = (args) => TwigFooter4(args);
